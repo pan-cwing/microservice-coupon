662468b8c824dfdb61faae8ca1b2345a46ec1190
pan-cwing <71680051+pan-cwing@users.noreply.github.com>
2020-12-29 22:38:58 +0800

    项目初始化

---
+++ microservice-coupon/microservice-coupon-service/coupon-template/src/main/java/org/example/coupon/CouponTemplateApplication.java
@@ -0,0 +1,11 @@
+package org.example.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponTemplateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CouponTemplateApplication.class, args);
+    }
+}
